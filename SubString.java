eaafa29a8ffd664b6833526cc917f13956049c83
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-09-26 14:49:32 +0530

    Creates the SubString from existing string

--- SubString.java
+++ SubString.java
@@ -1,3 +1,4 @@
+//Write a program to create a substring of given string with start index and last index by using the following method of java.lang.String class. public java.lang.String substring(int, int);
 public class SubString {
    public static void main(String[] args) {
         String s="hello dinesh";
