fb0723c544b89d0b02204f5ddb7afd54c5f6fff6
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-09-26 12:56:00 +0530

    String to char array using toCharArray method

---
+++ StrToCharArray.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class StrToCharArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any String");
+        String s=sc.nextLine();
+        char[] c =s.toCharArray();
+        
+        System.out.println(c[0]);
+    }
+}
