c482dd5b5e7e757f327455e1e1b6e02bae591c2a
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-09-26 14:47:34 +0530

    Creates the SubString from existing string

---
+++ SubString.java
@@ -0,0 +1,7 @@
+public class SubString {
+   public static void main(String[] args) {
+        String s="hello dinesh";
+        String rs=s.substring(6,12);
+          System.out.println(rs);
+   } 
+}
