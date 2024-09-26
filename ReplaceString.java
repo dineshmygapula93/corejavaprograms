e453e2f530c2533bcabea940e01386ea5e6fdca2
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-09-26 14:39:35 +0530

    replace the string

---
+++ ReplaceString.java
@@ -0,0 +1,8 @@
+//Write a program to replace characters in a given string by using the following method of java.lang.String class. public java.lang.String replace(char, char);
+public class ReplaceString {
+    public static void main(String[] args) {
+      String s="hello dinesh";
+      String rs=s.replace("hello","hi");
+        System.out.println(rs);
+    }
+}
