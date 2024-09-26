83eb95f4e4fa1041df9f626d2ef993ceb2344f05
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-09-26 14:56:41 +0530

    Removes the extra spaces from String

--- RemoveSpace.java
+++ RemoveSpace.java
@@ -1,3 +1,4 @@
+//Write a program to remove extra spaces at the begining of the text and end of the text by using the following method of java.lang.String class. public java.lang.String trim();
 public class RemoveSpace {
     public static void main(String[] args) {
         String s="                                       hello dinesh  ";
