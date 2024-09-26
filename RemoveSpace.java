f8d622b991658471772717b82337719ee160c694
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-09-26 14:55:15 +0530

    Removes the extra spaces from String

---
+++ RemoveSpace.java
@@ -0,0 +1,7 @@
+public class RemoveSpace {
+    public static void main(String[] args) {
+        String s="                                       hello dinesh  ";
+        String rs=s.trim();
+          System.out.println(rs);
+   }
+}
