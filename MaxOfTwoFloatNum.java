c2a6b6cc7cc921d8042557b4af7352b3a55124ad
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-01 19:22:30 +0530

    max of two numbers using the float class

---
+++ MaxOfTwoFloatNum.java
@@ -0,0 +1,9 @@
+// Write a program to find the maximum of two floating point numbers by using the following method of java.lang.Float class.public static float max(float, float);
+public class MaxOfTwoFloatNum {
+    public static void main(String[] args) {
+        Float f1=243.23f;
+        Float f2=543.243f;
+        Float rf=Float.max(f1,f2 );
+        System.out.println("The max of the two numbers is "+rf);
+    }   
+}
