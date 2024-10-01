f20d4f310ea22de53a35ab9563b6e24d68fddd0c
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-01 19:18:52 +0530

    Adding two floating point numbers using Float Class

---
+++ AddTwoNumFloatClass.java
@@ -0,0 +1,10 @@
+//Write a program to add two floating point numbers by using the following method of java.lang.Float class.public static float sum(float, float);
+
+public class AddTwoNumFloatClass {
+public static void main(String[] args) {
+    Float f1=243.23f;
+    Float f2=543.243f;
+    Float rf=Float.sum(f1,f2 );
+    System.out.println("The sum of the numbers is "+rf);
+}    
+}
