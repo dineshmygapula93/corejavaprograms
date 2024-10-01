669b501df274ebbe298d0e6cc1175016ec44069b
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-01 16:21:31 +0530

    To convert decimal to hexadecimal

---
+++ DecToHexa.java
@@ -0,0 +1,15 @@
+/**
+ * Write a program to convert decimal number to hexadecimal number by using
+the following method of java.lang.Integer class.
+  public static java.lang.String toHexString(int);
+ */
+import java.util.Scanner;
+public class DecToHexa {
+    public static void main(String[] args) {
+        System.out.println("Enter any Decimal number :");
+        Scanner sc =new Scanner(System.in);
+        int num=sc.nextInt();
+        String rs=Integer.toHexString(num);
+        System.out.println("The hexadecimal for number "+num+" is "+rs);
+    }
+}
