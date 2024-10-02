dc29d72b07fa8293956c3d53d4b554ee52023779
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-02 16:12:08 +0530

    To check  it is lower case or not

---
+++ IsLowerCase.java
@@ -0,0 +1,20 @@
+//Write a program to check whether given character is lower case letter or notby using the following method of java.lang.Character class.public static boolean isLowerCase(char);
+import java.util.Scanner;
+public class IsLowerCase {
+ public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter any letter :");
+    String s=sc.nextLine();
+    char [] ch=s.toCharArray();
+    boolean b=false;
+    for (char c : ch) {
+        b=Character.isLowerCase(c);  
+    }
+    if (b==true) {
+        System.out.println("It has lower case");
+    }else{
+        System.out.println("It has no lower case");
+    }
+    sc.close();
+ }  
+}
