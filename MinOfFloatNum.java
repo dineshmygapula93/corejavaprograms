960eb1b8b305c172e7581cd6b74717ad07aca655
dineshmygapula <dineshmygapula.mca@gmail.com>
2024-10-02 11:04:22 +0530

    min of two numbers using the float class

--- MinOfFloatNum.java
+++ MinOfFloatNum.java
@@ -1,7 +1,7 @@
 //Write a program to find the minimum of two floating point numbers by using the following method of java.lang.Float class.public static float min(float, float);
 public class MinOfFloatNum {
     public static void main(String[] args) {
-        Float f1=243.23f;
+        Float f1=242.23f;
         Float f2=543.243f;
         Float rf=Float.min(f1,f2 );
         System.out.println("The min of the two numbers is "+rf);
